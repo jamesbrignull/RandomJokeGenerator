82fc1ff1f88a36f40ece18612c02d27e9ed1faec
James Brignull <70067372+jamesbrignull@users.noreply.github.com>
2020-09-26 10:52:17 +0100

    Add files via upload

---
+++ RandomJokeGenerator/script.js
@@ -0,0 +1,32 @@
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
+let punchline;
+
+const getPunchline = () => {
+    punchlineDiv.innerHTML = punchline;
+    punchlineDiv.classList.add('bubble');
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+}
+
+punchlineBtn.addEventListener('click', getPunchline);
+
+newJokeBtn.addEventListener('click', getJoke);
+
+async function getJoke() {
+    const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+    const joke =  await jokePromise.json();
+    
+    setupDiv.innerHTML = joke[0].setup;
+    punchline = joke[0].punchline;
+
+    punchlineDiv.innerHTML = "";
+    punchlineDiv.classList.remove('bubble');
+
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+} 
+
+getJoke();
